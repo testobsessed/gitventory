6e889dd888e6b0d8802cb33574d32591ca728320
Elisabeth Hendrickson <esh@qualitytree.com>
2012-12-15 19:40:24 -0800

    request github auth

--- app.rb
+++ app.rb
@@ -3,3 +3,11 @@ require 'sinatra'
 get '/hi' do
   "Hello World!"
 end
+
+get '/login' do
+  redirect "https://github.com/login/oauth/authorize?client_id=943a62469e4b2721bebe&redirect_uri=http://gitventory.cloudfoundry.com/authed"
+end
+
+get '/auth' do
+  "Were you redirected here?"
+end
