b4a0ec2af78f12dcfdc3aacef7948b5339a29f20
Elisabeth Hendrickson <esh@qualitytree.com>
2012-12-16 10:19:21 -0800

    Not pretty, but auth actually works now.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source :rubygems
 gem 'rack'
 gem 'sinatra', :require => 'sinatra/base'
 gem 'haml'
+gem 'github_api'
 
 group :development do
   gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,13 +1,39 @@
 require 'sinatra'
+require 'net/https'
+require 'open-uri'
+require 'github_api'
+
+# note that the secret key for this app is set as an environment variable
+# if the app stops working, double check that SECRET_KEY is set and is
+# correct according to the app settings in github.
+
+set :secret_key, ENV['SECRET_KEY']
+github = Github.new :client_id => "943a62469e4b2721bebe", :client_secret => settings.secret_key
 
 get '/hi' do
-  "Hello World!"
+  "Hello World! Your secret is #{settings.secret_key}"
 end
 
 get '/login' do
-  redirect "https://github.com/login/oauth/authorize?client_id=943a62469e4b2721bebe&redirect_uri=http://gitventory.cloudfoundry.com/authed"
+  redirect github.authorize_url
 end
 
-get '/auth' do
-  "Were you redirected here?"
+get '/limit' do
+  "#{github.ratelimit}"
 end
+
+get '/say' do
+  "It said #{params[:response]}"
+end
+
+get '/' do
+  if (params.count == 1 && params["code"])
+    authorization_code = params[:code]
+    token = github.get_token( authorization_code )
+    github = Github.new(:oauth_token => token.token)
+    "Auth code was #{authorization_code}. Token was #{token.token}. Rate limit is now #{github.ratelimit}. Github can see repos for cloudfoundry?"
+  else
+    "GARDEN SQUIRRELS! " + params.to_s
+  end
+end
+
